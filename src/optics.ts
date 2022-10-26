0da71717c77d319b600f2d39b6918fa8a88bd479
MBauerDC <33489577+MBauerDC@users.noreply.github.com>
2022-10-26 18:53:49 +0200

    change comments

--- src/optics.ts
+++ src/optics.ts
@@ -3,6 +3,7 @@ import { Maybe } from "./monads.ts"
 module dcAG.functionalTS.optics {
 
     // LENS - for getting and updating in immutable data-structures
+    // (reifies the "has-a"-concept)
 
     interface Lens<I, P, PR, O> {
         get(i: I): P
@@ -10,8 +11,8 @@ module dcAG.functionalTS.optics {
         compose<P2, P2R>(l2: Lens<P, P2, P2R, PR>): Lens<I, P2, P2R, O>
     }
 
-    // INNERLENS - for getting and upting within immutable data-structures without changing the schema
-    
+    // INNERLENS - for getting and updating within immutable data-structures (product-types) without changing the schema
+        
     interface InnerLens<I, P> extends Lens<I, P, P, I> {}
     
     function lens<I, P, PR, O>(p: (i:I) => P, u: (i:I, pr: PR) => O): Lens<I, P, PR, O> {
@@ -25,6 +26,10 @@ module dcAG.functionalTS.optics {
     }
     
     function innerLens<I, P>(p: (i:I) => P, u: (i:I, p: P) => I): InnerLens<I, P> { return lens(p, u) }
+
+
+   // PRISM - for getting and updating within sum-types
+   // (reifies the "is-a"-concept)
 
     interface Prism<S, P> {
         getMaybe(s: S): Maybe<P>
